3bd103be450b0aaee38560b0fe98861abb9dcb2b
brunacardoz0 <122402169+brunacardoz0@users.noreply.github.com>
2023-09-30 09:50:44 -0300

    Delete site directory

--- site/script_home.js
+++
@@ -1,5 +0,0 @@
-let usuario = JSON.parse(localStorage.getItem("logdo"));
-document.getElementById("titulo").innerHTML = "Bem vindo, "+usuario.login+"!";
-function deslogar(){
- window.location.href = "index.html"
-}
